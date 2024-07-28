75886d293e856d286d39f7f0c698236763f4cf71
B190007A <67599101+B190007A@users.noreply.github.com>
2024-07-28 09:59:10 +0800

    Add files via upload

---
+++ TypeConversionExamples.py
@@ -0,0 +1,15 @@
+print(bool(0))
+print(bool(0.1))
+print(bool("afsdafsd"))
+ptint(bool("False"))
+
+# print(int("fadsfasf"))
+print(int("1"))
+print(int(0.5))
+print(int(False))
+print(int(True))
+
+print(float("1"))
+print(float(0.5))
+print(float(False))
+print(float(True))
---
+++ Variable.py
@@ -0,0 +1,27 @@
+is_game_over = False
+print(is_game_over)
+is_game_over = True
+print(is_game_over)
+is_game_over = 5 > 6
+print(is_game_over)
+
+num_lives = 5 
+print(num_lives)
+percent_health = 0.5
+print(percent_health)
+
+player_name = "Nimish" 
+print(player_name)
+player_name = "Mammoth"
+print(player_name)
+
+print(type(is_game_over))
+print(type(num_lives))
+print(type(percent_health))
+print(type(player_name))
+
+num_lives = 5
+# num_lives = "5"
+str_num_lives = str(num_lives)
+print(type(num_lives))
+print(type(str_num_lives))
